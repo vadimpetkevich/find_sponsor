f954da1a0ec63425e023497038e4c9920fb1b0d3
Vadim Petkevich <petkevich.minsk@gmail.com>
2015-03-10 20:55:20 +0300

    Improve Investor's show view

--- app/models/businessman.rb
+++ app/models/businessman.rb
@@ -6,6 +6,6 @@ class Businessman < ActiveRecord::Base
 
   has_attached_file :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
-    :default_url => "/images/:style/missing.png"
+    :default_url => "/images/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/, :size => { :less_than => 1.megabytes }
 end
